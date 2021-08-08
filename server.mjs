cc7ef1c8491fbb3a9b22f52ada01667b0f53f920
ablause <46499306+ablause@users.noreply.github.com>
2021-08-08 12:46:15 +0200

    Migrate http2 to Express

--- server.mjs
+++ server.mjs
@@ -1,107 +1,65 @@
-import fs from 'fs'
-import path from 'path'
-import http2 from 'http2'
-import mime from 'mime-types'
+import express from 'express'
 import nodemailer from 'nodemailer'
+import compression from 'compression'
+import path from 'path'
 
-const prefixAPI = '/api'
-const publicRoot = './public'
-
-const options = {
-  origins: [process.env.HOST ?? 'https://localhost/'],
-  allowHTTP1: true
-}
-
-if (process.env.SSL_CERT !== undefined && process.env.SSL_KEY !== undefined) {
-  Object.assign(options, {
-    cert: fs.readFileSync(process.env.SSL_CERT),
-    key: fs.readFileSync(process.env.SSL_KEY)
-  })
-}
-
-const server = http2.createSecureServer(options)
+const server = express()
+const publicRoot = './dist'
 
 const smtp = nodemailer.createTransport({
   port: 465,
   secure: true,
-  host: process.env.SMTP_HOST,
+  host: String(process.env.SMTP_HOST),
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    user: String(process.env.SMTP_USER),
+    pass: String(process.env.SMTP_PASS)
   }
 })
 
-// Static files
-server.on('stream', (stream, headers) => {
-  const pathname = headers[http2.constants.HTTP2_HEADER_PATH]
+const mailOptions = {
+  subject: 'Contact Form Submission',
+  to: process.env.SMTP_USER
+}
 
-  if (pathname.startsWith(prefixAPI)) return
+server.use(compression())
+server.use(express.static(publicRoot))
+server.use(express.json())
 
-  let fullPath = path.join(publicRoot, pathname)
-
-  let responseMimeType = mime.lookup(fullPath)
-
-  if (responseMimeType === false) {
-    fullPath = path.resolve(publicRoot, 'index.html')
-    responseMimeType = 'text/html'
-  }
-
-  stream.respondWithFile(fullPath, { 'content-type': responseMimeType }, {
-    onError: (err) => {
-      if (err.code === 'ENOENT') {
-        stream.respond({ ':status': http2.constants.HTTP_STATUS_NOT_FOUND })
-      } else {
-        stream.respond({ ':status': http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR })
-      }
-      stream.end()
-    }
-  })
+server.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH')
+  res.setHeader('Access-Control-Allow-Credentials', false)
+  next()
 })
 
-// API
-server.on('request', (req, res) => {
-  if (!req.url.startsWith(prefixAPI)) return
+server.get('*', (_, res) => {
+  res.sendFile(path.resolve(publicRoot, 'index.html'))
+})
 
-  let body
+server.post('/api/contact', (req, res) => {
+  const { name, email, message } = req.body
 
-  req.on('data', (chunk) => {
-    body = JSON.parse(chunk.toString())
-  })
-
-  req.on('end', () => {
-    switch ([req.method, req.url].join(' ')) {
-      case 'POST /api/contact': {
-        const { name, email, message } = body
-
-        const html = `
+  const html = `
                 <p>Name: ${name}</p>
                 <p>Email: ${email}</p>
                 <p>Message:\n ${message}</p>
                 `
 
-        smtp.sendMail({ from: email, subject: 'Contact Form Submission', to: process.env.SMTP_USER, html })
-          .then(() => res.writeHead(http2.constants.HTTP_STATUS_OK, 'Message Sent'))
-          .catch(() => res.writeHead(http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, 'ERROR'))
-          .finally(() => res.end())
-
-        break
-      }
-      default:
-        res.writeHead(http2.constants.HTTP_STATUS_NOT_FOUND)
-        res.end()
-        break
+  smtp.sendMail({ from: email, html, ...mailOptions }, (err) => {
+    if (err) {
+      res.status().json({ name: err.name, message: err.message })
     }
+
+    res.status(200)
   })
 })
 
-server.listen(Number(process.env.PORT ?? 443), () => {
-  try {
-    smtp.verify()
+server.listen(Number(process.env.PORT), () => {
+  smtp.verify((err) => {
+    if (err) throw new Error(err)
 
-    console.log('Server is ready!')
-  } catch (error) {
-    server.close(error)
-  }
+    console.log(`Server is available on ${process.env.HOST}:${process.env.PORT}`)
+  })
 })
 
 export default server
