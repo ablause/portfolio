30a3f625d72f45dc8a62945f55320f7994e37300
ablause <46499306+ablause@users.noreply.github.com>
2021-09-13 14:55:30 +0200

    Change informations for github

--- src/views/AboutView/AboutView.tsx
+++ src/views/AboutView/AboutView.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import classNames from 'classnames'
 
 import { Section } from '../../components'
@@ -6,51 +6,61 @@ import styles from './AboutView.module.css'
 
 // export interface AboutViewProps {}
 
-const AboutView = forwardRef<HTMLElement>((_, ref) => (
-  <Section id='about' title='About Me' subtitle='My introduction' ref={ref}>
-    <div className={classNames(styles.container, 'container grid')}>
-      <img className={styles.img} width={250} height={120} src='assets/person-coding.svg' alt='about-me' />
+const AboutView = forwardRef<HTMLElement>((_, ref) => {
+  const [data, setData] = useState<null | Record<string, any>>(null)
 
-      <div className={styles.data}>
-        <p className={styles.description}>
-          Hi everyone, my name is Alexandre Blause and I live in Brussels, Belgium. I'm passionate about what I do and code regularly for my personal projects. I'm currently looking for a job.
-        </p>
+  useEffect(() => {
+    fetch('https://api.github.com/users/ablause')
+      .then(async (res) => await res.json())
+      .then((result) => setData(result), (err) => console.error(err))
+  }, [])
 
-        <div className={styles.info}>
-          <div>
-            <span className={styles['info-title']}>
-              02+
-            </span>
-            <span className={styles['info-name']}>
-              project <br /> Repository
-            </span>
-          </div>
-          <div>
-            <span className={styles['info-title']}>
-              8+
-            </span>
-            <span className={styles['info-name']}>
-              Completed <br /> project
-            </span>
-          </div>
-          <div>
-            <span className={styles['info-title']}>
-              01+
-            </span>
-            <span className={styles['info-name']}>
-              Repositories <br /> pull-request
-            </span>
-          </div>
-        </div>
+  return (
+    <Section id='about' title='About Me' subtitle='My introduction' ref={ref}>
+      <div className={classNames(styles.container, 'container grid')}>
+        <img className={styles.img} width={250} height={120} src='assets/person-coding.svg' alt='about-me' />
 
-        <div className={styles.buttons}>
-          <a className='button button--flex' download href='assets/documents/alexandre-blause-cv.pdf'>
-            Download CV <i className='uil uil-download-alt button__icon' />
-          </a>
+        <div className={styles.data}>
+          <p className={styles.description}>
+            Hi everyone, my name is Alexandre Blause and I live in Brussels, Belgium. I'm passionate about what I do and code regularly for my personal projects. I'm currently looking for a job.
+          </p>
+
+          <div className={styles.info}>
+            <div>
+              <span className={styles['info-title']}>
+                {data?.public_repos ?? '00'}+
+              </span>
+              <span className={styles['info-name']}>
+                Github <br /> Repositories
+              </span>
+            </div>
+            <div>
+              <span className={styles['info-title']}>
+                02+
+              </span>
+              <span className={styles['info-name']}>
+                Completed <br /> project
+              </span>
+            </div>
+            <div>
+              <span className={styles['info-title']}>
+                03+
+              </span>
+              <span className={styles['info-name']}>
+                Open-source <br /> pull-request
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.buttons}>
+            <a className='button button--flex' download href='assets/documents/alexandre-blause-cv.pdf'>
+              Download CV <i className='uil uil-download-alt button__icon' />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </Section>
-))
+    </Section>
+  )
+})
 
 export default AboutView
